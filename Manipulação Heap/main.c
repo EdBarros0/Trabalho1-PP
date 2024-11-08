719c28eeaf01006f9fc502e1820a3b03a3e9b4e5
Eduardo Almeida <eduardo.hevangelista@gmail.com>
2024-11-08 23:09:00 +0000

    Atualização Lista

--- Manipulação Heap/main.c
+++ Manipulação Heap/main.c
@@ -10,6 +10,16 @@ typedef struct Area{
 
 } Area;
 
+typedef struct Cheio{
+
+    int nome;
+    int comeco;
+    int tamanho;
+    struct Cheio *prox;
+
+} Cheio;
+
+
 Area* InserirNo(Area* a, int comeco, int tamanho ){
 
     
@@ -27,20 +37,114 @@ Area* InserirNo(Area* a, int comeco, int tamanho ){
     return a;
 }
 
+void *organiza_heap (int heap[], Area *lista, int cord){
+    
+    if(lista != NULL){
+        if(cord < 20){
+            for(int i = lista->comeco; i< (lista->tamanho + lista->comeco -1); i++){
+                heap[i] = 0;
+            }
+            organiza_heap(heap,lista->prox,lista->tamanho + lista->comeco -1);
+        }
+    }
+}
+int Search_menor(Area *lista, int x){
+    if(lista == NULL){
+        return 20;
+    }else{
+        if(lista->tamanho >= x)
+            return (Search_menor(lista->prox, x) < lista->tamanho)? Search_menor(lista->prox, x) : lista->tamanho;
+        else
+            return Search_menor(lista->prox, x);
+    }
+}
+int Search_maior(Area *lista, int x){
+     if(lista == NULL){
+        return 0;
+    }else{
+        if(lista->tamanho >= x)
+            return (Search_maior(lista->prox, x) > lista->tamanho)? Search_maior(lista->prox, x) : lista->tamanho;
+        else
+            return Search_maior(lista->prox, x);
+    }
+}
+int Search_primeiro(Area * lista, int x){
+    if(lista == NULL){
+        return 20;
+    }else{
+        if(lista->tamanho >= x)
+            return (Search_primeiro(lista->prox, x) < lista->comeco)? Search_primeiro(lista->prox, x) : lista->comeco;
+        else
+            return Search_primeiro(lista->prox, x);
+    }
+}
+
+Area *best_type(Area *lista, int x){
+    if(lista != NULL){
+        if(Search_menor(lista, x) == lista->tamanho){
+            lista->tamanho = lista->tamanho - x;
+            if(lista->tamanho == 0){
+                Area *no = (Area*)malloc(sizeof(Area));
+                no == lista->prox;
+                return no;
+            }
+            lista->comeco = lista->comeco + x;
+        }else
+            return lista->prox;
+    }
+    return lista;
+}
+
+Area *worst_type(Area *lista, int x){
+    if(lista != NULL){
+        if(Search_maior(lista, x) == lista->tamanho){
+            lista->tamanho = lista->tamanho - x;
+            if(lista->tamanho == 0){
+                Area *no = (Area*)malloc(sizeof(Area));
+                no == lista->prox;
+                return no;
+            }
+            lista->comeco = lista->comeco + x;
+        }else
+            return lista->prox;
+    }
+    return lista;
+}
+
+Area *first_type(Area *lista, int x){
+    if(lista != NULL){
+        if(Search_primeiro(lista, x) == lista->comeco){
+            lista->tamanho = lista->tamanho - x;
+            if(lista->tamanho == 0){
+                Area *no = (Area*)malloc(sizeof(Area));
+                no == lista->prox;
+                return no;
+            }
+            lista->comeco = lista->comeco + x;
+        }else
+            return lista->prox;
+    }
+    return lista;
+}
+
 
 int main()
 {  
     Area* lista = (Area*)malloc(sizeof(Area));
     lista = NULL;
     lista = InserirNo(lista,0,20);
+    Cheio* espaco = (Cheio*)malloc(sizeof(Cheio));
     
-    printf("%d\n\n",lista->tamanho);
-
     while (1) {
-
-                            //printf("\x1B[2J\x1b[H");
-    printf("Heap");
-    printf("O que deseja fazer ?");
+    int heap[20];
+                               //printf("\x1B[2J\x1b[H");
+    printf("                Heap\n");
+    for(int i=0;i<20;i++){
+        heap[i]=1;
+    }
+    organiza_heap(heap,lista,0);
+    printf("\n\n");
+    printf("O que deseja fazer ?\n");
     break;
 
 
